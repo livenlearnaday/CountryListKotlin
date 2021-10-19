a6581812fd0f156be3de13b9e00eef05143dc5fe
livenlearnaday <livenlearnaday@gmail.com>
2021-10-20 06:32:11 +0700

    remove redundant not null assertion

--- app/src/main/java/io/github/livenlearnaday/countrylistkotlin/ui/countries/adapter/CountriesAdapter.kt
+++ app/src/main/java/io/github/livenlearnaday/countrylistkotlin/ui/countries/adapter/CountriesAdapter.kt
@@ -73,7 +73,7 @@ class CountriesAdapter @Inject constructor(
 
             }
 
-            itemBinding.favCheckbox.isChecked = country.favorite!!
+            itemBinding.favCheckbox.isChecked = country.favorite
 
 
         }
