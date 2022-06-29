d5de599b7e940ace40f41262fbf7a9c1dfb0621c
livenlearnaday <livenlearnaday@gmail.com>
2022-06-29 23:37:11 +0700

    removed unused import and unused constants

--- app/src/main/java/io/github/livenlearnaday/countrylistkotlin/utils/Constants.kt
+++ app/src/main/java/io/github/livenlearnaday/countrylistkotlin/utils/Constants.kt
@@ -3,5 +3,3 @@ package io.github.livenlearnaday.countrylistkotlin.utils
 
 
 const val BASE_URL = "https://livenlearnaday.github.io/data/countries/"
-const val SHARED_PREFERENCES_KEY = "io.github.livenlearnaday.countrylistkotlin"
-const val COUNTRIES_LOADED_KEY = "COUNTRIES_LOADED_KEY"
